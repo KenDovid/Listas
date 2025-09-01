013f6957a2af4a6c2ca24311c152957f9bfc1116
David Romero <davidaar05@gmail.com>
2025-08-31 23:36:10 -0500

    Add files via upload

--- Listas/factura2.py
+++ Listas/factura2.py
@@ -5,16 +5,16 @@ print("======================================")
 veg = ["Tomates", "Cebollas", "Pimentones", "Lechugas", "Brócoli", "Zanahorias", "Papas", "Pepinos"]
 val = [1000, 1500, 2000, 3000, 5000, 1200, 800, 1800]
 
-# Mostrar catálogo
+
 print("Lista de productos disponibles:")
 for i in range(len(veg)):
     print(f"{i+1}. {veg[i]} - {val[i]}$")
 
 print("======================================")
 
-carrito = [0] * len(veg)  # lista inicializada en ceros
+carrito = [0] * len(veg)  
 
-# Pedimos compras
+
 continuar = "s"
 while continuar.lower() == "s":
     opcion = int(input("Seleccione el número del vegetal que desea comprar: "))
@@ -26,7 +26,7 @@ while continuar.lower() == "s":
 
     continuar = input("¿Desea comprar otro producto? (s/n): ")
 
-# Factura
+
 print("======================================")
 print("Factura Electrónica")
 print("Supermercado Verdulero")
@@ -34,7 +34,7 @@ print("======================================")
 
 resultado = [x * y for x, y in zip(carrito, val)]
 for vegetal, unidades, precio, subtotal in zip(veg, carrito, val, resultado):
-    if unidades > 0:  # solo muestra los que compró
+    if unidades > 0:  
         print(f"{vegetal}: {unidades} x {precio}$ = {subtotal}$")
 
 total = sum(resultado)
